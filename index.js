93ed82f29af46cdcb1c4709ea43e7a08e2b094e0
Rajarshi <ghoshal.rajarshi@gmail.com>
2019-11-14 10:57:05 +0530

    added post for /signup

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
+//middelwares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+//database
+var db = null;
+var url = 'mongodb://localhost:27017';
+MongoClient.connect(url, function (err, client) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connection established to', url);
+      db = client.db('meme-hub');
+      var collection = db.collection('users');
+      collection.find({}).toArray(function(err,res){
+		// console.log(err);
+		// console.log(res);
+	})
+    //   client.close();
+    }
+  });
+
+//routes
+app.post('/signup',function(req,response){
+	var data = req.body;
+	var collection = db.collection('users');
+	collection.insertOne(data,function(err,res){
+		if (err){
+			console.log(err);
+		}
+		console.log();
+		return response.send('created user');
+    })
+    
+	
+	// console.log(collection);
+});  
 
 
 app.listen(9091, function(){

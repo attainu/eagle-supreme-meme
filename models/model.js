094124270d849ba3f45e2748479dbef83a123d64
kartik-tomar-vst-au4 <kartik9896@gmail.com>
2019-11-15 12:10:11 +0530

    sign-up (no repeat entry)

--- models/model.js
+++ models/model.js
@@ -12,24 +12,33 @@ MongoClient.connect(url, function (err, client) {
         console.log('Connection established to', url);
         db = client.db('meme-hub');
         var collection = db.collection('accounts');
-        collection.find({}).toArray(function (err, res) {
-            // console.log(err);
-           //  console.log(res);
-        })
-        //   client.close();
+        collection.find({}).toArray(function (err, res) {})
     }
 });
 
 
 AuthModel.singUp = function (req, res, cb) {
-   //  console.log(req);
+    //  console.log(req);
     var collection = db.collection('accounts');
-    collection.insertOne(req, function (error, response) {
-        if (error) {
-            return cb(error)
+    collection.find({}).toArray(function (err, response) {
+        console.log(response);
+        var user = null;
+        response.forEach(function (value, index) {
+            if (value.userId === req.userId) {
+                user = value;
+                return cb("user already exists")
+            }
+        })
+        if (user === null) {
+            collection.insertOne(req, function (error, response) {
+                if (error) {
+                    return cb(error)
+                } else {
+                    return cb(null, "created user")
+                }
+            });
         }
-        return cb(null, "created user")
-    });
+    })
 }
 
 
@@ -37,7 +46,7 @@ AuthModel.signIn = function (req, session, cb) {
 
     var collection = db.collection('accounts');
     collection.find({}).toArray(function (err, response) {
-       // console.log(req);
+        // console.log(req);
         if (!err) {
             var user = null;
             response.forEach(function (value, index) {

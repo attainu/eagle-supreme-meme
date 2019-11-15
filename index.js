a7f0a3c4c0b10cf8cd64f70eafa476f5b1559c45
Bipul Yadav <krbipul.90@gmail.com>
2019-11-15 12:38:07 +0530

    Admin Chnages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
-// const session = require('express-session');
+const session = require('express-session');
 
 //middelwares
 app.use(express.static('public'));
@@ -17,20 +17,20 @@ app.engine('.hbs', exphbs({
 }));
 app.set('view engine', '.hbs');
 
-// login Session
-// app.use(session({
-//   name: "assignment",
-//   secret: 'someRandomStuff',
-//   resave: true,
-//   saveUninitialized: true,
-//   cookie: {
-//     httpOnly: true,
-//     maxAge: 120000,
-//     path: '/',
-//     sameSite: true,
-//     secure: false
-//   }
-// }))
+
+app.use(session({
+  name: "assignment",
+  secret: 'someRandomStuff',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: 120000,
+    path: '/',
+    sameSite: true,
+    secure: false
+  }
+}));
 
 var link = [{
     "image": "https://i.kym-cdn.com/photos/images/newsfeed/001/248/399/430.png"
@@ -45,6 +45,10 @@ var link = [{
     "image": "https://www.todaysparent.com/wp-content/uploads/2017/06/when-your-kid-becomes-a-meme-1024x576-1497986561.jpg"
   }
 ]
+var temp = {
+  "username":"admin",
+  "password":"admin"
+};
 
 // controller
 var authRoute = require('./routes/route.js');
@@ -62,8 +66,46 @@ app.get('/', function (req, res) {
 app.get('/loginpage', function (req, res) {
   res.render('login');
 })
+
 app.get('/admin',function(req,res){
-    res.send('Admin Pannel');
+    res.render('adminlogin',{
+      layout:"admin.hbs"
+    });
+})
+app.post('/auth',function(req,res){
+  if(temp.username==req.body.username && temp.password==req.body.password){
+    req.session.user=true;
+    return res.json({status:"200",message:"success"});
+    
+    
+  }
+  return res.json({status:"402",message:"Invalid"});
+})
+app.get('/dashboard',function(req,res){
+  if(req.session.user){
+    return res.render('dashboard',{
+      layout:"admin.hbs"
+    })
+  }
+  return res.redirect('/admin');
+
+  console.log(req.body);
+  
+})
+app.get('/adminlogout',function(req,res){
+  if(req.session.user){
+    req.session.destroy(function(err){
+      if(err){
+        next(err)
+      }else{
+        res.clearCookie('assignment')
+        
+        return res.redirect('/admin');
+      }
+    })
+  }
+
+  else return res.redirect('/admin');
 })
 
 app.listen(9091, function () {

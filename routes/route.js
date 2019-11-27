9990157c894687c16ec685839c2242c1b9ea2543
kartik-tomar-vst-au4 <kartik9896@gmail.com>
2019-11-27 16:27:45 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -10,7 +10,7 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 var db = null;
 var pass = encodeURIComponent('meme-hub@6')
-var url = 'mongodb+srv://root:'+pass+'@meme-hub-vxtlu.mongodb.net/meme-hub?retryWrites=true&w=majority';
+var url = 'mongodb+srv://root:' + pass + '@meme-hub-vxtlu.mongodb.net/meme-hub?retryWrites=true&w=majority';
 MongoClient.connect(url, function (err, client) {
     if (err) {
         console.log('Unable to connect to the mongoDB server. Error:', err);
@@ -38,14 +38,10 @@ authController.checkIfLoggedIn = function (req, res, next) {
 
     //   console.log("check session " + req.session.user);
     //  console.log("Url " + req.originalUrl);
-    if (req.originalUrl !== '/logoutpage' && req.originalUrl !== '/upload') {
-        return next();
+    if (req.originalUrl === '/logoutpage' || req.originalUrl === '/upload') {
+        return res.redirect("/loginpage");
     } else {
-        if (typeof req.session.user === "undefined") {
-            return res.redirect("/loginpage");
-        } else {
-            return next();
-        }
+        return next();
     }
 }
 
@@ -84,7 +80,7 @@ authController.like = function (req, res) {
                     exists = false
                 }
                 if (response[i].accountId === req.session.user[0]._id) {
-                  //  console.log("3.1");
+                    //  console.log("3.1");
                     if (response[i].postId === req.body.postId) {
                         //   console.log("3")
                         exists = true
@@ -203,24 +199,24 @@ authController.saveComment = function (req, res) {
 var urlLink;
 authController.upload = function (req, response) {
     console.log(req.body);
-    
+
     //cloudinary
     var collection = db.collection('approval_pending');
     cloudinary.uploader.upload(req.file.path, function (error, res) {
-        console.log("this the cloudinary Error"+ error)
+        console.log("this the cloudinary Error" + error)
         if (!error) {
             urlLink = res.url;
             var finalImg = {
-                title : req.body.title,
-                tag : req.body.tag,
-                likes : 0,
+                title: req.body.title,
+                tag: req.body.tag,
+                likes: 0,
                 url: urlLink,
-                name:req.session.user[0].userName,
-                userupload:"true"
+                name: req.session.user[0].userName,
+                userupload: "true"
             };
             console.log("res>>", res.url);
             collection.insertOne(finalImg, function (error, result) {
-               console.log("inserting error"+ error)
+                console.log("inserting error" + error)
                 if (error) {
                     return error
                 }
@@ -239,26 +235,28 @@ authController.home = function (req, res) {
     var request = req;
     var response = res;
 
-    Model.checkLike(request, response, function (err, doc) {                 //   check like  
+    Model.checkLike(request, response, function (err, doc) { //   check like  
         if (!err) {
-          //  console.log(doc);
-           var posts = []
+            //  console.log(doc);
+            var posts = []
             Model.home(function (error, success) {
-             //   console.log(success);
-             
-             if (doc){
-                success.forEach(element => {
-                    doc.forEach(id =>{
-                        if(JSON.stringify(id) === JSON.stringify(element._id)){
-                            element.show = true
-                           // console.log(element);
-                        } //else {console.log(element._id)}
-                    }) 
-                    posts.push(element)
-                });
-            } else { posts = success}
-           // console.log(posts);
-           
+                //   console.log(success);
+
+                if (doc) {
+                    success.forEach(element => {
+                        doc.forEach(id => {
+                            if (JSON.stringify(id) === JSON.stringify(element._id)) {
+                                element.show = true
+                                // console.log(element);
+                            } //else {console.log(element._id)}
+                        })
+                        posts.push(element)
+                    });
+                } else {
+                    posts = success
+                }
+                // console.log(posts);
+
                 if (error) {
                     return res.render('home', {
                         data: error
@@ -286,26 +284,28 @@ authController.whatsnew = function (req, res) {
     var request = req;
     var response = res;
 
-    Model.checkLike(request, response, function (err, doc) {                 //   check like  
+    Model.checkLike(request, response, function (err, doc) { //   check like  
         if (!err) {
-          //  console.log(doc);
-           var posts = []
+            //  console.log(doc);
+            var posts = []
             Model.whatsnew(function (error, success) {
-             //   console.log(success);
-             
-             if (doc){
-                success.forEach(element => {
-                    doc.forEach(id =>{
-                        if(JSON.stringify(id) === JSON.stringify(element._id)){
-                            element.show = true
-                           // console.log(element);
-                        } //else {console.log(element._id)}
-                    }) 
-                    posts.push(element)
-                });
-            } else { posts = success}
-           // console.log(posts);
-           
+                //   console.log(success);
+
+                if (doc) {
+                    success.forEach(element => {
+                        doc.forEach(id => {
+                            if (JSON.stringify(id) === JSON.stringify(element._id)) {
+                                element.show = true
+                                // console.log(element);
+                            } //else {console.log(element._id)}
+                        })
+                        posts.push(element)
+                    });
+                } else {
+                    posts = success
+                }
+                // console.log(posts);
+
                 if (error) {
                     return res.render('whatsnew', {
                         data: error
@@ -531,11 +531,17 @@ authController.adminDelete = function (req, res) {
 authController.wishList = function (req, res) {
     if (req.session.user) {
         var collection = db.collection('wishlist');
-        collection.find({$and:[{accountId:req.session.user[0]._id},{postId:req.body.id}]}).toArray(function(err,success){
-            if(err){
+        collection.find({
+            $and: [{
+                accountId: req.session.user[0]._id
+            }, {
+                postId: req.body.id
+            }]
+        }).toArray(function (err, success) {
+            if (err) {
                 return res.send(err);
             }
-            if(success.length<1){
+            if (success.length < 1) {
                 collection.insertOne({
                     accountId: req.session.user[0]._id,
                     accountName: req.session.user[0].userName,
@@ -543,14 +549,12 @@ authController.wishList = function (req, res) {
                 });
                 return res.send("Saved");
 
-            }
-            else return res.send("Already Saved");
+            } else return res.send("Already Saved");
         })
-       
+
         //console.log(req.session.user);
-        
-    }
-     else return res.send("Login");
+
+    } else return res.send("Login");
 }
 // authController.getWishList =  function (req, res) {
 //     if (req.session.user) {
@@ -565,14 +569,14 @@ authController.wishList = function (req, res) {
 //             }
 //             else {
 //                 //var x;
-                
+
 //             //console.log(success);
 //             success.forEach(function(data){
 //                 collection2.find({_id:ObjectID(data.postId)}).toArray(function(err,suc){
 //                     if(suc){
 //                         //x=suc;
 //                         console.log("suc",suc);
-                        
+
 //                         database.push(suc[0]);
 //                         console.log("This is DataBase",database);
 //                         }
@@ -583,8 +587,8 @@ authController.wishList = function (req, res) {
 //             });
 //             console.log("520 Databse",database);
 //         }
-            
-            
+
+
 //         })
 //        // return res.send(database);
 
@@ -622,7 +626,7 @@ authController.getWishList = function (req, res) {
                             logIn: "<a href='/logoutpage'>Logout</a>"
                         });
                     }
-                    
+
                 });
             }
         })

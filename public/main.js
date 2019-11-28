77e82db40a611f1c947724305c7180344fb09015
kartik-tomar-vst-au4 <kartik9896@gmail.com>
2019-11-28 23:05:25 +0530

    upload funcionality

--- public/main.js
+++ public/main.js
@@ -6,81 +6,81 @@ $(document).ready(function () {
         $('#classChangeT').attr('class', 'twitter-share-button');
 
     })
-    $(document).ready(function () {
-        $('.loader').fadeOut(2900);
-        // $('.heart').on('click',function(){
-        //     $(this).addClass('broken');
-        // 		  $(this).toggleClass('happy broken');
 
-        //         });
-        $('.showComments').on('click', function () {
-            $(this).css('background', 'none');
-        });
-        $('.showComments').on('focus', function () {
-            $(this).css('outline', 'none');
+    $('.loader').fadeOut(2900);
+    // $('.heart').on('click',function(){
+    //     $(this).addClass('broken');
+    // 		  $(this).toggleClass('happy broken');
+
+    //         });
+    $('.showComments').on('click', function () {
+        $(this).css('background', 'none');
+    });
+    $('.showComments').on('focus', function () {
+        $(this).css('outline', 'none');
+    })
+    $('.flagImage').on('click', function () {
+        var id = $(this).attr('alt')
+        console.log(id);
+        $.ajax({
+            url: "/report",
+            data: {
+                "id": id
+            },
+            method: "post",
+            success: function (data) {
+                console.log("report" + data);
+                $("#reportDone").click();
+
+            }
         })
-        $('.flagImage').on('click', function () {
-            var id = $(this).attr('alt')
-            console.log(id);
-            $.ajax({
-                url: "/report",
-                data: {
-                    "id": id
-                },
-                method: "post",
-                success: function (data) {
-                    console.log("report" + data);
-                    $("#reportDone").click();
-
+    })
+    $('.postwishlist').on('click', function () {
+        var id = $(this).attr('id')
+        console.log(id);
+        $.ajax({
+            url: "/wishlist",
+            data: {
+                "id": id
+            },
+            method: "post",
+            success: function (data) {
+                console.log(data);
+                if (data == "Login") {
+                    $("#loginModal").click()
                 }
-            })
+
+
+            }
         })
-        $('.postwishlist').on('click', function () {
-            var id = $(this).attr('id')
-            console.log(id);
-            $.ajax({
-                url: "/wishlist",
-                data: {
-                    "id": id
-                },
-                method: "post",
-                success: function (data) {
-                    console.log(data);
-                    if (data == "Login") {
-                        $("#loginModal").click()
-                    }
+    })
+    $('.hamburger').on('click', function () {
+        console.log("Clicked");
+        $('.change').toggleClass('changeTO');
+        $('.line').toggleClass('lineChange');
+    });
 
 
-                }
-            })
-        })
-        $('.hamburger').on('click',function(){
-            console.log("Clicked");
-            $('.change').toggleClass('changeTO');
-            $('.line').toggleClass('lineChange');
-        });
 
+    $('#uploadModalBtn').on('click', async function () {
 
-        var log;
-        $('#uploadModalBtn').on('click', async function () {
+        $.ajax({
+            url: "/checkLogin",
+            method: "post",
+            data: "check login",
+            success: function (response) {
+                console.log(response)
+                log = response
 
-            $.ajax({
-                url: "/checkLogin",
-                method: "post",
-                data: "check login",
-                success: function (response) {
-                    console.log(response)
-                    log = response
-                    
-                    $("#btnUpForm").click(function (event) {
+                $("#btnUpForm").click(function (event) {
 
-                        event.preventDefault();
-                        var form = $('#uploadForm')[0];
-                        var data = new FormData(form);
+                    event.preventDefault();
+                    var form = $('#uploadForm')[0];
+                    var data = new FormData(form);
 
-                        $("#btnUpForm").prop("disabled", true);
-                        if (response === ('loggedIn')){
-                            console.log(response)
+                    $("#btnUpForm").prop("disabled", true);
+                    if (response === ('loggedIn')) {
+                        console.log(response)
                         $.ajax({
                             type: "POST",
                             enctype: 'multipart/form-data',
@@ -91,7 +91,7 @@ $(document).ready(function () {
                             cache: false,
                             timeout: 600000,
                             success: function (data) {
-                                if(data === "done"){
+                                if (data === "done") {
                                     $("#BtnClose").click();
                                     alert('Post Uploaded')
                                 }
@@ -102,64 +102,66 @@ $(document).ready(function () {
                             }
                         });
                     } else {
-                        console.log("hello"+response)
+                        console.log("hello" + response)
                         $("#BtnClose").click();
                         $('#loginModal').click();
                     }
-                    });
-                
-                }
-            })
+                });
+
+            }
         })
     })
 
-    // $("#uploadForm").submit(function(e) {
-    //     e.preventDefault();
-    //     var formData = new FormData(this);    
+    $('#uploadModalBtn2').on('click', async function () {
 
-    //     $.post($(this).attr("action"), formData, function(data) {
-    //         alert(data);
-    //     });
-    // });
+        $.ajax({
+            url: "/checkLogin",
+            method: "post",
+            data: "check login",
+            success: function (response) {
+                console.log(response)
+                log = response
 
-    // $("#btnUpForm").click(function (event) {
+                $("#btnUpForm").click(function (event) {
+
+                    event.preventDefault();
+                    var form = $('#uploadForm')[0];
+                    var data = new FormData(form);
+
+                    $("#btnUpForm").prop("disabled", true);
+                    if (response === ('loggedIn')) {
+                        console.log(response)
+                        $.ajax({
+                            type: "POST",
+                            enctype: 'multipart/form-data',
+                            url: "/upload",
+                            data: data,
+                            processData: false,
+                            contentType: false,
+                            cache: false,
+                            timeout: 600000,
+                            success: function (data) {
+                                if (data === "done") {
+                                    $("#BtnClose").click();
+                                    alert('Post Uploaded')
+                                }
+                            },
+                            error: function (e) {
+                                console.log("ERROR : ", e);
+
+                            }
+                        });
+                    } else {
+                        console.log("hello" + response)
+                        $("#BtnClose").click();
+                        $('#loginModal').click();
+                    }
+                });
+
+            }
+        })
+    })
 
 
-    //     event.preventDefault();
-    //     var form = $('#uploadForm')[0];
-    //     var data = new FormData(form);
 
-    //     $("#btnUpForm").prop("disabled", true);
-
-    //     $.ajax({
-    //         type: "POST",
-    //         enctype: 'multipart/form-data',
-    //         url: "/upload",
-    //         data: data,
-    //         processData: false,
-    //         contentType: false,
-    //         cache: false,
-    //         timeout: 600000,
-    //         success: function (data) {
-    //             console.log(data);
-    //         },
-    //         error: function (e) {
-    //             console.log("ERROR : ", e);
-
-    //         }
-    //     });
-
-    // });
-
-
-    //     $.ajax({
-    //         type: "POST",
-    //         url: window.location.pathname,
-    //         data: formData, // serializes the form's elements.
-    //         success: function (response) {
-    //             console.log(response)
-    //         }
-    //     });
-
-    // })
 });
